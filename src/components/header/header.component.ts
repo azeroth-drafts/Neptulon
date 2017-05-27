018286b0d540f26f29965527b5582b398f77caad
Mechoshi Puhanaga <mechokio@gmail.com>
2017-05-27 19:07:34 +0300

    Code cleaning in Header component

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -1,8 +1,6 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
 
-import { LoginData } from './../../models';
-
 @Component( {
     name: 'header',
     template: require( './header.component.html' ),
@@ -18,10 +16,6 @@ export class HeaderComponent extends Vue {
 
     private messageWelcome: string = 'Welcome ';
 
-    private user: LoginData = {
-        username: '',
-        password: ''
-    }
 
     set welcome( newValue: string ) {
         this.$welcome = newValue;
