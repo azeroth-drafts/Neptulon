f345d1202de92eab6ca80cf4a50d6e06e11aa032
Milen Donchev <milen_d@abv.bg>
2017-05-27 21:31:48 +0300

    remove unnecessary params in emitter

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,22 +1,13 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
 
-import commonServiceInstance from './../../services/common.service';
-
 @Component( {
     name: 'home',
     template: require( './home.component.html' ),
 } )
 export class HomeComponent extends Vue {
-    private commonService = commonServiceInstance;
 
     private joinGame(): void {
-        let token = this.commonService.getToken();
-        this.$emit( 'join',
-            {
-                token: token
-            }
-        );
-        console.log('join game pressed');
+        this.$emit( 'join' );
     }
 }
--- src/components/waiting/waiting.component.ts
+++ src/components/waiting/waiting.component.ts
@@ -1,8 +1,6 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
 
-import commonServiceInstance from './../../services/common.service';
-
 @Component( {
     name: 'waiting',
     template: require( './waiting.component.html' ),
@@ -12,27 +10,17 @@ import commonServiceInstance from './../../services/common.service';
 } )
 export class WaitingComponent extends Vue {
     private isJoined: boolean;
-    private commonService = commonServiceInstance;
     private token: string;
 
     constructor() {
         super();
-        this.token = this.commonService.getToken();
     }
 
     private acceptGame(): void {
-        this.$emit( 'acceptGame',
-            {
-                token: this.token
-            }
-        );
+        this.$emit( 'acceptGame' );
     }
 
     private rejectGame(): void {
-        this.$emit( 'rejectGame',
-            {
-                token: this.token
-            }
-        );
+        this.$emit( 'rejectGame' );
     }
 }
